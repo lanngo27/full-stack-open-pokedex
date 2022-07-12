3a201e8e0e0842371fa0e0deafa43da145acf424
Lan Ngo <ntthanhlan27994@gmail.com>
2022-07-13 00:37:53 +0200

    Fixed lint failure

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -7,7 +7,7 @@ describe('Pokedex', function() {
 
   it('pokemon page can be navigated to', function() {
     cy.visit('http://localhost:5000')
-    
+
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
     cy.contains('overgrow')
